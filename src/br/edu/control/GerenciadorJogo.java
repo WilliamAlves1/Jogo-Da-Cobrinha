6f6c61dd5bc94ca5ef84eaf9296f8261886c9b42
CamilleVitoria-27 <camille.p2709@gmail.com>
2026-06-04 17:26:45 -0300

    Adiciona interface do jogo com GamePanel e GerenciadorJogo

---
+++ src/br/edu/control/GerenciadorJogo.java
@@ -0,0 +1,71 @@
+package br.edu.control;
+
+import ModeloCobra.Cobrinha;
+import ModeloCobra.Comida;
+import CobraListaEncadeada.No;
+import br.edu.view.GamePanel;
+
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GerenciadorJogo extends KeyAdapter implements ActionListener {
+
+    private Cobrinha cobrinha;
+    private Comida comida;
+    private GamePanel gamePanel;
+    private Timer timer;
+    private int direcaoAtual = 1;
+
+    public GerenciadorJogo(Cobrinha cobrinha, Comida comida, GamePanel gamePanel) {
+        this.cobrinha = cobrinha;
+        this.comida = comida;
+        this.gamePanel = gamePanel;
+
+        this.timer = new Timer(130, this);
+        this.timer.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (cobrinha.isViva()) {
+            int indexCabeca = cobrinha.getCorpo().getTamanho() - 1;
+            No cabeca = cobrinha.getCorpo().get(indexCabeca);
+
+            boolean comeuComida = (cabeca.getX() == comida.getX() && cabeca.getY() == comida.getY());
+
+            cobrinha.mover(direcaoAtual, comeuComida);
+
+            if (comeuComida) {
+                comida.gerarNovaPosicao(cobrinha.getCorpo());
+            }
+
+            if (!cobrinha.isViva()) {
+                timer.stop();
+            }
+        }
+
+        gamePanel.repaint();
+    }
+
+    //pega as setas do teclado para mudar a direção da cobrinha
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int codigoTecla = e.getKeyCode();
+
+        if ((codigoTecla == KeyEvent.VK_UP) && (direcaoAtual != 2)) {
+            direcaoAtual = 0;
+        }
+        if ((codigoTecla == KeyEvent.VK_RIGHT) && (direcaoAtual != 3)) {
+            direcaoAtual = 1;
+        }
+        if ((codigoTecla == KeyEvent.VK_DOWN) && (direcaoAtual != 0)) {
+            direcaoAtual = 2;
+        }
+        if ((codigoTecla == KeyEvent.VK_LEFT) && (direcaoAtual != 1)) {
+            direcaoAtual = 3;
+        }
+    }
+}
